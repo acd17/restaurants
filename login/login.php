841bca3b07280d03acdb1ea4ec15c61793d96158
Vinik <56496329+Vn1k@users.noreply.github.com>
2023-10-13 22:07:02 +0700

    Create login.php

---
+++ login/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+<body>
+    <Form:post>
+        <table>
+            <tr>
+                <td>EMAIL</td><br>
+                <td><input type="text" value="email"></td><br>
+            </tr>
+            <tr>
+                <td>PASSWORD</td><br>
+                <td><input type="password" value="password"></td>
+            </tr>
+            <button type="submit"></button>
+        </table>
+    </Form:post>
+</body>
+</html>
