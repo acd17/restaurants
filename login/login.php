47d9bc100e4478254431230a87494b868b629c50
Vn1k <56496329+Vn1k@users.noreply.github.com>
2023-10-15 02:15:32 +0700

    LOGIN design
    BESOK PROSES_LOGIN NYA

--- login/login.php
+++ login/login.php
@@ -1,23 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="../style.css">
     <title>Login Form</title>
 </head>
 <body>
-    <Form:post>
-        <table>
-            <tr>
-                <td>EMAIL</td><br>
-                <td><input type="text" value="email"></td><br>
-            </tr>
-            <tr>
-                <td>PASSWORD</td><br>
-                <td><input type="password" value="password"></td>
-            </tr>
-            <button type="submit"></button>
-        </table>
-    </Form:post>
+    <div class="container">
+        <div class="login-box">
+            <h2>Login</h2>
+            <form:post>
+                <div class="user-box">
+                    <input type="text" name="" required="">
+                    <label>Email</label>
+                </div>
+                <div class="user-box">
+                    <input type="password" name="" required="">
+                    <label>Password</label>
+                </div>
+                <div class="text-box">
+                    <p>Don't have an account yet? <br>
+                    <a href="#">register here</a>
+                    </p>
+                </div>
+                <a href="#" class="button-login">Login</a>
+            </form>
+        </div>
+    </div>
 </body>
+<script type="text/javascript" src="../node_modules/tw-elements/dist/js/tw-elements.umd.min.js">
+</script>
+
 </html>
