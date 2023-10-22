cbf3232e4fd13032e3e64599de82ed1e283c17cf
Vn1k <56496329+Vn1k@users.noreply.github.com>
2023-10-22 23:32:51 +0700

    update direct

--- src/check-login.php
+++ src/check-login.php
@@ -27,13 +27,22 @@ if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['role
         if(mysqli_num_rows($result) === 1) {
             //the username must be unique
             $row = mysqli_fetch_assoc($result);
-            if ($row['password'] === $password && $row['role'] == $role) {
-                 $_SESSION['name'] = $row['name'];
-                 $_SESSION['password'] = $row['password'];
-                 $_SESSION['id'] = $row['id'];
-                 $_SESSION['username'] = $row['username'];
-
-                 header("location: home.php");
+            if ($row['password'] === $password) {
+                if ($row['role'] == 'admin') {
+                    // Pengguna adalah admin, arahkan ke halaman admin
+                    $_SESSION['name'] = $row['name'];
+                    $_SESSION['password'] = $row['password'];
+                    $_SESSION['id'] = $row['id'];
+                    $_SESSION['username'] = $row['username'];
+                    header("location: admin_dashboard.php");
+                } else {
+                    // Pengguna adalah pengguna biasa, arahkan ke halaman pengguna
+                    $_SESSION['name'] = $row['name'];
+                    $_SESSION['password'] = $row['password'];
+                    $_SESSION['id'] = $row['id'];
+                    $_SESSION['username'] = $row['username'];
+                    header("location: user_dashboard.php");
+                }
             }else {
                 header("location: loginUser.php?error=Incorrect Username and Password");
             }
